be705ffeee21afdccce829c4f22e695cfb5fe2ba
Rob <robbiejohnwallace@yahoo.co.uk>
2022-08-01 21:01:26 +0100

    Started implemeint header styles

--- personal-website-web/src/components/header/Header.tsx
+++ personal-website-web/src/components/header/Header.tsx
@@ -3,12 +3,16 @@ import { useTheme } from '../../hooks';
 
 const Header = () => {
   const {toggleTheme} = useTheme();
+  const links = ['About', 'Projects', 'CV', 'Contact Me'];
 
   return (
-    <>
-      <h2> Header </h2>
-      <button onClick={() => toggleTheme()}></button>
-    </>
+    <div className='header flex row jc-center width-100'>
+      { links.map(link => 
+        <div className='element mlr-1em'>
+          {link}
+        </div>
+      )}
+    </div>
   );
 };
 
