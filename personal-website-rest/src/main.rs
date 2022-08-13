4864eedf53be5eb04d703af6962160f4f0dfcff3
Rob <robbiejohnwallace@yahoo.co.uk>
2022-08-13 14:55:32 +0100

    Inital MongoDB Setup

--- personal-website-rest/src/main.rs
+++ personal-website-rest/src/main.rs
@@ -1,28 +1,31 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+extern crate dotenv;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
+use dotenv::dotenv;
+use std::env;
+use std::error::Error;
+use tokio;
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+   dotenv().ok();
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+   // Load the MongoDB connection string from an environment variable:
+   let client_uri =
+      env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+   // A Client is needed to connect to MongoDB:
+   // An extra line of code to work around a DNS issue on Windows:
+   let options =
+      ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
+         .await?;
+   let client = Client::with_options(options)?;
+
+   // Print the databases in our MongoDB cluster:
+   println!("Databases:");
+   for name in client.list_database_names(None, None).await? {
+      println!("- {}", name);
+   }
+
+   Ok(())
 }
